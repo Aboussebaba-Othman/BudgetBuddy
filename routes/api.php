a24bdc641d3c3fba7450f2e5f320c3569ca0ec5c
John Doe <johndoe@example.com>
2025-03-12 12:25:08 +0100

    fix error update tags&exponse

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\TagController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+
 
 Route::post('register',[AuthController::class, 'register']);
 Route::post('login',[AuthController::class, 'login']);
